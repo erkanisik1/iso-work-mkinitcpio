88d81f273198c1c676eddcaeed0ff58eb0d1f131
erkanisik1 <erkanisik@yahoo.com>
2022-03-15 17:29:24 +0300

    Update mkgrubx64.sh

--- pisiman/data/mkgrubx64.sh
+++ pisiman/data/mkgrubx64.sh
@@ -33,4 +33,5 @@ done
 # grub2-kbdcomp -o en.gkb en
 
 # grub2-mkstandalone --locale-directory=/boot/grub2/locale/ -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi --themes="pisi" -o $efi_path "boot/grub/grub.cfg=./grub.cfg" -v
-grub2-mkstandalone -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi --fonts="/boot/grub2/fonts/unicode.pf2" --locale-directory=/usr/share/locale/ --themes=$theme -o $efi_path "boot/grub/grub.cfg=./grub.cfg" -v
+#grub2-mkstandalone -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi --fonts="/boot/grub2/fonts/unicode.pf2" --locale-directory=/usr/share/locale/ --themes=$theme -o $efi_path "boot/grub/grub.cfg=./grub.cfg" -v
+grub2-mkstandalone -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi --fonts="/boot/grub2/fonts/unicode.pf2"  --themes=$theme -o $efi_path "boot/grub/grub.cfg=./grub.cfg" -v
